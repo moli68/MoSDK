fc307fbedc7eed69d112a020a2cabff997609e89
Zeoy <312391869@qq.com>
2019-04-29 11:12:54 +0800

    Signed-off-by: Z 新增点击图片以及点击文字 新增圆角图片显示自定义
    Signed-off-by: Zeoy <312391869@qq.com>

---
+++ molisdk/src/main/java/com/moli68/library/custom/PressedImageView.java
@@ -0,0 +1,40 @@
+package com.moli68.library.custom;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+public class PressedImageView extends android.support.v7.widget.AppCompatImageView {
+    private float scaleSize;//按压颜色
+
+    public PressedImageView(Context context) {
+        super(context);
+        this.scaleSize = 0.97f;
+    }
+
+    public PressedImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.scaleSize = 0.97f;
+    }
+
+    public PressedImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.scaleSize = 0.97f;
+    }
+
+    @Override
+    public void setPressed(boolean pressed) {
+        super.setPressed(pressed);
+        if (isPressed()) {
+            setScaleX(this.scaleSize);
+            setScaleY(this.scaleSize);
+        } else {
+            setScaleX(1.0f);
+            setScaleY(1.0f);
+        }
+    }
+
+    public void setScaleSize(float scaleSize) {
+        this.scaleSize = scaleSize;
+    }
+}
